191adfb73b30f5b6a97707b33266afcd7e0977b8
Richard Cooper <rcooper.xelaj@protonmail.com>
2026-05-08 11:21:58 +0200

    added github pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'quenbyako', // Usually your GitHub org/user name.
   projectName: 'cynosure-docs', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
